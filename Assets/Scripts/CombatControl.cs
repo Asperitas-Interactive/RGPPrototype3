345e6fa808af400eb26de97695ffe8778f1dc8a5
Pikaboi <braden.cleur@gmail.com>
2021-04-07 15:49:30 +1200

    woaahh

--- Assets/Scripts/CombatControl.cs
+++ Assets/Scripts/CombatControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CombatControl : MonoBehaviour
 {
     public Animator animator;
+    public int damage = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,26 +21,34 @@ public class CombatControl : MonoBehaviour
         if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank") && x > 0.0f)
         {
             animator.SetTrigger("Stab");
+            damage = 30;
         } 
         //This will make the uppercut attack happen
         else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank") && x < 0.0f)
         {
             animator.SetTrigger("RisingSlash");
+            damage = 50;
         } 
         //This will do the first hit of a combo
         else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank"))
         {
             animator.SetTrigger("SwordSlash");
+            damage = 10;
         }
         //This will do the second hit of the combo
         else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("SwordSlash"))
         {
             animator.SetTrigger("SwordSlash2");
+            damage = 20;
         }
         //This will do the final hit of the combo
         else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("SwordSlash2"))
         {
             animator.SetTrigger("SwordSlash3");
+            damage = 30;
+        } else
+        {
+            damage = 0;
         }
 
 
--- Assets/Scripts/enemyHealth.cs
+++ Assets/Scripts/enemyHealth.cs
@@ -6,10 +6,22 @@ public class enemyHealth : MonoBehaviour
 {
     private int health = 100;
 
+    private void Update()
+    {
+        if(health < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Sword")
         {
+            CombatControl cc = collision.gameObject.GetComponentInParent<CombatControl>();
+
+            health -= cc.damage;
+
             Debug.Log(health);
         }
     }
