cedfa15ce77d11deacd77c8ba76a173bae3dde21
Pikaboi <braden.cleur@gmail.com>
2021-04-07 14:35:00 +1200

    animation attacks

--- Assets/Scripts/CombatControl.cs
+++ Assets/Scripts/CombatControl.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class CombatControl : MonoBehaviour
 {
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Melee"))
+        float x = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank") && x >= 0.5f)
         {
-            Debug.Log("Activate Melee Attack");
+            animator.SetTrigger("Stab");
+        } 
+        else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank") && x <= -0.5f)
+        {
+            animator.SetTrigger("RisingSlash");
+        } 
+        else if (Input.GetButtonDown("Melee") && animator.GetCurrentAnimatorStateInfo(0).IsName("Blank"))
+        {
+            animator.SetTrigger("SwordSlash");
         }
     }
 }
