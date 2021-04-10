8dd1a76319fc0427439ecfe857d2d3f618c4cee9
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-04-10 15:24:53 +1200

    woah

---
+++ Assets/Scripts/PickUpSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour
+{
+    public GameObject pickup;
+
+    private float timer = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0.0f)
+        {
+            //Update this for our area size
+            Vector3 pos = new Vector3(Random.Range(-100, 101), 2.0f, Random.Range(-100, 101));
+            Instantiate(pickup, pos, Quaternion.Euler(0, 0, 0));
+        }
+    }
+}
